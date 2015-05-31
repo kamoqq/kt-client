d1c7a793078ba898212bbcbdcdc6e1122fb5b90e
kamoqq <kamoqq@gmail.com>
2015-05-31 15:18:04 +0900

    Fixed test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-/*global describe, it, beforeEach */
+/*global describe, it, beforeEach*/
+/*jshint expr: true*/
 'use strict';
 
 var expect = require('chai').expect;
@@ -153,7 +154,7 @@ describe('kt-client', function() {
       client.set('test_key', 'test_value', options, function(error) {
         expect(error).to.be.undefined;
         client.get('test_key', function(error, value, expire) {
-          expect(value.to.equal('test_value'));
+          expect(value).to.equal('test_value');
           expect(expire).to.be.an.instanceof(Date);
           expect(error).to.be.undefined;
           done();
